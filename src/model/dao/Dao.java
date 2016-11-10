5f8a2af4e09727d668da8b02c4099f2f6dd9c2e8
KNAZANGA <knazanga@gmail.com>
2016-11-10 11:10:19 +0100

    Dao modified

--- src/model/dao/Dao.java
+++ src/model/dao/Dao.java
@@ -9,17 +9,14 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class Dao<T> {
-	
+
 	Session session;
 
 	public Dao() {
 		Configuration configuration = new Configuration().configure();
-
 		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
 				.applySettings(configuration.getProperties()).build();
-
 		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
 		session = sessionFactory.openSession();
 	}
 
@@ -28,22 +25,19 @@ public class Dao<T> {
 		session.save(entity);
 		session.getTransaction().commit();
 	}
-	
-	public T get(Class<T> objClass, long id){
-		session.beginTransaction();
-		T entity =(T) session.get(objClass, id);
-		session.getTransaction().commit();
-		return entity;
+
+	public T get(Class<T> objClass, long id) {
+		return (T) session.get(objClass, id);
+
 	}
-	
-	public List<T> findAll(Class<T> objClass){
-		session.beginTransaction();
-		List<T> all= session.createQuery("from "+objClass.getName()).list();
-		session.getTransaction().commit();
-		return all;
+
+	public List<T> findAll(Class<T> objClass) {
+
+		return session.createQuery("from " + objClass.getName()).list();
+
 	}
-	
-	public void delete(T entity){
+
+	public void delete(T entity) {
 		session.beginTransaction();
 		session.delete(entity);
 		session.getTransaction().commit();
--- src/model/dao/UserDao.java
+++ src/model/dao/UserDao.java
@@ -1,7 +1,5 @@
 package model.dao;
 
-import java.util.List;
-
 import org.hibernate.Query;
 
 import model.entities.User;
@@ -10,54 +8,38 @@ public class UserDao extends Dao<User> {
 
 	private final static UserDao SINGLETON = new UserDao();
 
-	private UserDao(){
-		super();
-	}
-
-	protected static UserDao getInstance(){
+	protected static UserDao getInstance() {
 		return SINGLETON;
 	}
 
 	public User getUserFromEmail(String email) {
-		session.beginTransaction();
 		Query query = session.createQuery("From User  u where u.email=:email");
 		query.setString("email", email);
-		List<User> list =query.list();
-		session.getTransaction().commit();
-		if (!list.isEmpty())
-			return list.get(0);
-		return null;
+		return (User) query.uniqueResult();
 	}
 
 	public User getUserFromPseudo(String pseudo) {
-		session.beginTransaction();
 		Query query = session.createQuery("From User u where u.pseudo=:pseudo");
 		query.setString("pseudo", pseudo);
-		List<User> list =query.list();
-		session.getTransaction().commit();
-		if (!list.isEmpty())
-			return list.get(0);
-		return null;
+		return (User) query.uniqueResult();
 	}
-	
-	public User getUserFromLogin(String pseudoOrEmail){
+
+	public User getUserFromLogin(String pseudoOrEmail) {
 		User u1 = getUserFromEmail(pseudoOrEmail);
-		if(u1 != null)
+		if (u1 != null)
 			return u1;
 		u1 = getUserFromPseudo(pseudoOrEmail);
 		return u1;
 	}
 
-	public boolean checkIfExists(String pseudo, String email){
+	public boolean checkIfExists(String pseudo, String email) {
 		User u1 = getUserFromEmail(email);
-		if(u1 != null)
+		if (u1 != null)
 			return true;
 		User u2 = getUserFromPseudo(pseudo);
-		if(u2 != null)
+		if (u2 != null)
 			return true;
 		return false;
 	}
-
-
 
 }
