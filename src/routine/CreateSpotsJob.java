ab33fdbb8e38f6980db28753c12b48e6dd6421ae
KNAZANGA <knazanga@gmail.com>
2016-11-10 16:34:39 +0100

    Creating spot bot modified

--- src/routine/CreateSpotsJob.java
+++ src/routine/CreateSpotsJob.java
@@ -1,45 +1,59 @@
 package routine;
 
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Random;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import model.dao.DaoFactory;
 import model.dao.UserDao;
 import model.entities.Address;
 import model.entities.Spot;
 import model.entities.User;
 
-import java.util.Random;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
 public class CreateSpotsJob implements Job {
+	public static final String GEOCODE_API = "https://maps.googleapis.com/maps/api/geocode/json?";
 
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 
-		UserDao userDao = DaoFactory.getInstance().getUserDao();
-
-		User bot = userDao.getUserFromLogin("bot1");
-
-		// TODO amine longitude,latitude inside PARIS
-
-		Double maxLong = 48.902833;
-		Double minLong = 48.815458;
-		Double maxLat = 2.416780, minLat = 2.245080;
+		Double maxLat = 48.902833;
+		Double minLat = 48.815458;
+		Double maxLong = 2.416780;
+		Double minLong = 2.245080;
 		Random rand = new Random();
 		Double longitude = new Double(((maxLong - minLong) * rand.nextDouble()) + minLong);
 		Double latitude = new Double(((maxLat - minLat) * rand.nextDouble()) + minLat);
+		String formattedAddr = "";
+		try {
 
-		// TODO check address using google api
+			URL url = new URL(GEOCODE_API + "latlng=" + latitude + "," + longitude);
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+			InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+			JsonParser parser = new JsonParser();
+			JsonObject jelement = parser.parse(isr).getAsJsonObject();
+			JsonArray results = jelement.getAsJsonArray("results");
+			JsonObject o = results.get(1).getAsJsonObject();
+			formattedAddr = o.getAsString();
+			System.out.println(formattedAddr);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		String formated_address = "55 rue bot";
+		//Address address = new Address(longitude, latitude, formattedAddr);
 
-		Address address = new Address(longitude, latitude, formated_address);
+		//Spot spot = new Spot(address, bot);
 
-		Spot spot = new Spot(address, bot);
-
-		DaoFactory.getInstance().getSpotDao().persist(spot);
-
+		//DaoFactory.getInstance().getSpotDao().persist(spot);
 	}
 
 }
